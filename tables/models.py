ce77b30b272a171c1efe1ca85e0e9aa76789a98c
Balazs9 <sempergerbalazs@gmail.com>
2022-01-15 13:48:51 +0000

    create Guest and Tables classes in models.py

--- tables/models.py
+++ tables/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+"""
+Guest booking
+"""
+
+
+class Guest(models.Model):
+    guest = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+
+
+"""
+Table / persons view
+"""
+
+
+class Tables(models.Model):
+    persons = models.IntegerField()
+    twochairs = models.IntegerField()
+    fourchairs = models.IntegerField()
+    party = models.IntegerField()
+    free = models.BooleanField(default=True)
+    book = models.ForeignKey(Guest, on_delete=models.CASCADE, name="reserve")
+
+    def __str__(self):
+        return (self.persons) + (self.book)
